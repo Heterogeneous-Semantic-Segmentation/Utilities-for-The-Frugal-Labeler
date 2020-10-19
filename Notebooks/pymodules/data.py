a3694ea9c5bcd319e140964bf92c55f3233dc7d8
Luca R <rettenbergerluca@gmail.com>
2020-10-19 15:15:56 +0200

    refactor + heteregenous mask iterator

--- Notebooks/pymodules/data.py
+++ Notebooks/pymodules/data.py
@@ -1,41 +1,31 @@
 from __future__ import print_function
 from keras.preprocessing.image import ImageDataGenerator
-import numpy as np 
+import numpy as np
 import os
 import glob
 import skimage.io as io
 import skimage.transform as trans
-from create_one_hot_encoded_map_from_mask import get_one_hot_map
-import tensorflow as tf
-import matplotlib.pyplot as plt
-from heterogeneous_mask_iterator import HeteregenousMaskIterator
+from heterogeneous_mask_iterator import HeterogeneousMaskIterator
 
 
-def adjustImage(img):
+def adjust_image(img):
     if np.max(img) > 1:
         img = img / 255.
     return img
 
 
-def adjustMask(mask, color_map, flag_multi_class):
-    if flag_multi_class:
-        one_hot_mask = []
-        for i in range(len(mask)):
-            one_hot_mask.append(get_one_hot_map(mask[i],color_map))
-        one_hot_mask = tf.stack(one_hot_mask)
-        return one_hot_mask
-    else:
-        # Requires the input image to only contain two colors, otherwise this will not work.
-        mask = mask / 255.
-        max_value = np.max(mask)
-        mask[mask >= max_value] = 1
-        mask[mask < max_value] = 0
-        return mask
-
-
-def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "rgb",
-                    mask_color_mode = "rgb",image_save_prefix  = "image",mask_save_prefix  = "mask",
-                    flag_multi_class = False,save_to_dir = None,target_size = (256,256),seed = 1,color_map=None):
+def train_generator(batch_size,
+                    train_path,
+                    image_folder,
+                    mask_folders,
+                    aug_dict,
+                    image_color_mode="rgb",
+                    mask_color_mode="rgb",
+                    image_save_prefix="image",
+                    mask_save_prefix="mask",
+                    save_to_dir=None,
+                    target_size=(256, 256),
+                    seed=1):
     '''
     can generate image and mask at the same time
     use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
@@ -43,92 +33,73 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
     '''
     image_datagen = ImageDataGenerator(**aug_dict)
     mask_datagen = ImageDataGenerator(**aug_dict)
-    image_generator = image_datagen.flow_from_directory(
-        train_path,
-        classes = [image_folder],
-        class_mode = None,
-        color_mode = image_color_mode,
-        target_size = target_size,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = image_save_prefix,
-        seed = seed)
-    mask_generator = mask_datagen.flow_from_directory(
-        train_path,
-        classes = [mask_folder],
-        class_mode = None,
-        color_mode = mask_color_mode,
-        target_size = target_size,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = mask_save_prefix,
-        seed = seed)
-
-    hetergenous_mask_generator = HeteregenousMaskIterator(train_path,
-                                                          mask_datagen,
-                                                          masks= ['ventral_mask_atrium', 'ventral_mask_bulbus', 'ventral_mask_heart'],
-                                                          color_map=color_map,
-                                                          color_mode = mask_color_mode,
-                                                          target_size = target_size,
-                                                          batch_size = batch_size,
-                                                          save_to_dir = save_to_dir,
-                                                          save_prefix  = mask_save_prefix,
-                                                          seed = seed)
-
-    train_generator = zip(image_generator, mask_generator,hetergenous_mask_generator)
-    for (img,mask,test) in train_generator:
-
-        plt.imshow(img[0])
-        plt.show()
-
-        plt.imshow(mask[0])
-        plt.show()
-        plt.imshow(test[0])
-        plt.show()
-
-        adjusted_img = adjustImage(img)
-        adjusted_mask = adjustMask(mask,color_map,flag_multi_class)
-        yield (adjusted_img, adjusted_mask)
+    image_generator = image_datagen.flow_from_directory(train_path,
+                                                        classes=[image_folder],
+                                                        class_mode=None,
+                                                        color_mode=image_color_mode,
+                                                        target_size=target_size,
+                                                        batch_size=batch_size,
+                                                        save_to_dir=save_to_dir,
+                                                        save_prefix=image_save_prefix,
+                                                        seed=seed)
+    heterogeneous_mask_generator = HeterogeneousMaskIterator(directory=train_path,
+                                                           image_data_generator=mask_datagen,
+                                                           masks=mask_folders,
+                                                           color_mode=mask_color_mode,
+                                                           target_size=target_size,
+                                                           batch_size=batch_size,
+                                                           save_to_dir=save_to_dir,
+                                                           save_prefix=mask_save_prefix,
+                                                           seed=seed)
+    train_generator = zip(image_generator, heterogeneous_mask_generator)
+    for (img, mask) in train_generator:
+        yield adjust_image(img), mask
 
 
-def testGenerator(test_path,num_image = 30,target_size = (256,256),flag_multi_class = False,as_gray = True):
+def test_generator(test_path, num_image=30, target_size=(256, 256), flag_multi_class=False, as_gray=True):
     '''
     can generate image and mask at the same time
     use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
     if you want to visualize the results of generator, set save_to_dir = "your path"
     '''
     for i in range(num_image):
-        img = io.imread(os.path.join(test_path, ("%d" % i).zfill(4) + ".tif"),as_gray = as_gray)
+        img = io.imread(os.path.join(test_path, ("%d" % i).zfill(4) + ".tif"), as_gray=as_gray)
         img = img / 255
         # https://github.com/zhixuhao/unet/issues/83
-        img = trans.resize(img,target_size)
-        img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
-        img = np.reshape(img,(1,)+img.shape)
+        img = trans.resize(img, target_size)
+        img = np.reshape(img, img.shape + (1,)) if (not flag_multi_class) else img
+        img = np.reshape(img, (1,) + img.shape)
         yield img
 
 
-def geneTrainNpy(image_path,mask_path,flag_multi_class = False,num_class = 2,image_prefix = "image",mask_prefix = "mask",image_as_gray = True,mask_as_gray = True):
-    image_name_arr = glob.glob(os.path.join(image_path,"%s*.png"%image_prefix))
+def gene_train_numpy(image_path,
+                     mask_path,
+                     flag_multi_class=False,
+                     num_class=2,
+                     image_prefix="image",
+                     mask_prefix="mask",
+                     image_as_gray=True,
+                     mask_as_gray=True):
+    image_name_arr = glob.glob(os.path.join(image_path, "%s*.png" % image_prefix))
     image_arr = []
     mask_arr = []
-    for index,item in enumerate(image_name_arr):
-        img = io.imread(item,as_gray = image_as_gray)
-        img = np.reshape(img,img.shape + (1,)) if image_as_gray else img
-        mask = io.imread(item.replace(image_path,mask_path).replace(image_prefix,mask_prefix),as_gray = mask_as_gray)
-        mask = np.reshape(mask,mask.shape + (1,)) if mask_as_gray else mask
-        img,mask = adjustImage(img, mask, flag_multi_class, num_class)
+    for index, item in enumerate(image_name_arr):
+        img = io.imread(item, as_gray=image_as_gray)
+        img = np.reshape(img, img.shape + (1,)) if image_as_gray else img
+        mask = io.imread(item.replace(image_path, mask_path).replace(image_prefix, mask_prefix), as_gray=mask_as_gray)
+        mask = np.reshape(mask, mask.shape + (1,)) if mask_as_gray else mask
+        img, mask = adjust_image(img, mask, flag_multi_class, num_class)
         image_arr.append(img)
         mask_arr.append(mask)
     image_arr = np.array(image_arr)
     mask_arr = np.array(mask_arr)
-    return image_arr,mask_arr
+    return image_arr, mask_arr
 
 
-def labelVisualize(num_class,color_dict,img):
-    img = img[:,:,0] if len(img.shape) == 3 else img
+def label_visualize(num_class, color_dict, img):
+    img = img[:, :, 0] if len(img.shape) == 3 else img
     img_out = np.zeros(img.shape + (3,))
     for i in range(num_class):
-        img_out[img == i,:] = color_dict[i]
+        img_out[img == i, :] = color_dict[i]
         print(color_dict[i])
     return img_out / 255
-
--- Notebooks/pymodules/heterogeneous_mask_iterator.py
+++ Notebooks/pymodules/heterogeneous_mask_iterator.py
@@ -1,18 +1,25 @@
 import numpy as np
 import os
-from keras_preprocessing.image.iterator import Iterator,BatchFromFilesMixin
+from keras_preprocessing.image.iterator import Iterator, BatchFromFilesMixin
 from keras_preprocessing.image.utils import (array_to_img,
-                    img_to_array,
-                    load_img)
-from create_one_hot_encoded_map_from_mask import get_one_hot_map
-import tensorflow as tf
+                                             img_to_array,
+                                             load_img)
 
-class HeteregenousMaskIterator(BatchFromFilesMixin, Iterator):
+
+def check_equal(iterator):
+    iterator = iter(iterator)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == rest for rest in iterator)
+
+
+class HeterogeneousMaskIterator(BatchFromFilesMixin, Iterator):
 
     def __init__(self,
                  directory,
                  image_data_generator,
-                 color_map=None,
                  target_size=(256, 256),
                  color_mode='rgb',
                  masks=None,
@@ -25,19 +32,21 @@ class HeteregenousMaskIterator(BatchFromFilesMixin, Iterator):
                  save_format='png',
                  subset=None,
                  interpolation='nearest',
-                 dtype='float32'):
-        super(HeteregenousMaskIterator, self).set_processing_attrs(image_data_generator,
-                                                                   target_size,
-                                                                   color_mode,
-                                                                   data_format,
-                                                                   save_to_dir,
-                                                                   save_prefix,
-                                                                   save_format,
-                                                                   subset,
-                                                                   interpolation)
+                 dtype='float32',
+                 background_color=None):
+        super(HeterogeneousMaskIterator, self).set_processing_attrs(image_data_generator,
+                                                                    target_size,
+                                                                    color_mode,
+                                                                    data_format,
+                                                                    save_to_dir,
+                                                                    save_prefix,
+                                                                    save_format,
+                                                                    subset,
+                                                                    interpolation)
+        self.background_color = background_color
         self.directory = directory
         self.dtype = dtype
-        self.color_map = color_map
+        self.target_size = target_size
         # First, count the number of samples and classes.
         self.num_classes = len(masks)
         self.class_indices = dict(zip(masks, range(len(masks))))
@@ -46,23 +55,13 @@ class HeteregenousMaskIterator(BatchFromFilesMixin, Iterator):
         # Check if all mask directories contain the same account of mask images
         for dirpath in (os.path.join(directory, subdir) for subdir in masks):
             results.append(os.listdir(dirpath))
-        if not self.checkEqual(results):
-            print('Mask mismatch!')
-            return
+        if not check_equal(results):
+            raise ValueError('Directories do not contain the same amount of masks (some directories have missing masks).')
         self.samples = len(results[0])
         self._mask_identifiers = results[0]
         self._mask_classes = masks
-        super(HeteregenousMaskIterator, self).__init__(self.samples, batch_size, shuffle, seed)
-        print('Found %d masks containing %d classes.' %(self.samples, len(masks)))
-
-    def checkEqual(self,iterator):
-        iterator = iter(iterator)
-        try:
-            first = next(iterator)
-        except StopIteration:
-            return True
-        return all(first == rest for rest in iterator)
-
+        super(HeterogeneousMaskIterator, self).__init__(self.samples, batch_size, shuffle, seed)
+        print('Found %d masks containing %d classes.' % (self.samples, len(masks)))
 
     @property
     def filepaths(self):
@@ -70,66 +69,28 @@ class HeteregenousMaskIterator(BatchFromFilesMixin, Iterator):
 
     @property
     def labels(self):
-        return self.classes
+        return self._mask_classes
 
     @property  # mixin needs this property to work
     def sample_weight(self):
         # no sample weights will be returned
         return None
 
-    def adjustMask(self,mask):
-        one_hot_mask = []
-        for i in range(len(mask)):
-            one_hot_mask.append(get_one_hot_map(mask[i], self.color_map))
-        one_hot_mask = tf.stack(one_hot_mask)
-        return one_hot_mask
-
-
-    def adj(self,mask):
-        one_hot_mask = []
-        for i in range(len(mask)):
-            one_hot_mask.append(get_one_hot_map(mask[i]))
-        one_hot_mask = tf.stack(one_hot_mask)
-        return one_hot_mask
-
-    def get_color_smallest_distance(self,color, color_map):
-        '''
-        Given a color (first parameter) this function gets the color from the color_map (second parameter) which is the
-        closest to the given color.
-        '''
-        smallest_dist = abs(sum(color) - sum(color_map[0]))
-        smallest_index = 0
-        for i in range(1, len(color_map)):
-            if abs(sum(color) - sum(color_map[i])) < smallest_dist:
-                smallest_dist = abs(sum(color) - sum(color_map[i]))
-                smallest_index = i
-        return color_map[smallest_index]
-
-    def get_one_hot_map(self,mask, class_index, background = [0, 0, 0]):
-        '''
-        Expects an numpy array of the mask. If no color_map is given, the color_map is generated from all colors in the mask.
-        '''
+    def get_one_hot_map(self, mask, class_index, background=None):
+        if background is None:
+            background = [0, 0, 0]
         mask = mask.copy()
-        img_width = mask.shape[1]
-        img_height = mask.shape[0]
-        one_hot_map = np.zeros((img_height, img_width, self.num_classes))
-        # if value is ambiguous take the smallest distance to next value in binary color map
-        for i in range(img_height):
-            for j in range(img_width):
-                img_val = mask[i, j, :].tolist()
-                if img_val == background:
-                    mask[i, j] = np.zeros(self.num_classes,dtype=int).tolist()
+        mask_width = mask.shape[1]
+        mask_height = mask.shape[0]
+        one_hot_map = np.zeros((mask_height, mask_width, self.num_classes))
+        for i in range(mask_height):
+            for j in range(mask_width):
+                mask_val = mask[i, j, :].tolist()
+                if mask_val == background:
+                    mask[i, j] = np.zeros(self.num_classes, dtype=int).tolist()
                 else:
-                    mask[i, j] = np.eye(self.num_classes,dtype=int)[class_index].tolist()
+                    mask[i, j] = np.eye(self.num_classes, dtype=int)[class_index].tolist()
         return mask
-        palette = self.color_map
-        one_hot_map = []
-        for colour in palette:
-            class_map = tf.reduce_all(tf.equal(mask, colour), axis=-1)
-            one_hot_map.append(class_map)
-        one_hot_map = tf.stack(one_hot_map, axis=-1)
-        one_hot_map = tf.cast(one_hot_map, tf.float32)
-        return one_hot_map
 
     def _get_batches_of_transformed_samples(self, index_array):
         """Gets a batch of transformed samples.
@@ -138,33 +99,39 @@ class HeteregenousMaskIterator(BatchFromFilesMixin, Iterator):
             index_array: Array of sample indices to include in batch.
 
         # Returns
-            A batch of transformed samples.
+            A batch of transformed samples in one-hot-encoded format.
         """
-        batch_x = np.zeros((len(index_array),) + self.image_shape, dtype=self.dtype)
+        batch_x = np.zeros((len(index_array),) + (self.target_size[0], self.target_size[1], self.num_classes),
+                           dtype=self.dtype)
         # build batch of image data
         # self.filepaths is dynamic, is better to call it once outside the loop
         filepaths = self.filepaths
         for i, j in enumerate(index_array):
+            one_hot_map = np.zeros((self.target_size[0], self.target_size[1], self.num_classes), dtype=np.float32)
             # Iterate over all classes
-            one_hot_arrays = np.zeros(())
+            params = None
             for k in range(self.num_classes):
-                img = load_img(os.path.join(self.directory,self._mask_classes[k],filepaths[j]),
+                img = load_img(os.path.join(self.directory, self._mask_classes[k], filepaths[j]),
                                color_mode=self.color_mode,
                                target_size=self.target_size,
                                interpolation=self.interpolation)
                 x = img_to_array(img, data_format=self.data_format)
-                one_hot_arrays.append(self.get_one_hot_map(x,k))
                 # Pillow images should be closed after `load_img`,
                 # but not PIL images.
                 if hasattr(img, 'close'):
                     img.close()
-            # Add arrays into one array
-            # CODE STILL MISSING
-            if self.image_data_generator:
-                params = self.image_data_generator.get_random_transform(x.shape)
-                x = self.image_data_generator.apply_transform(x, params)
-                x = self.image_data_generator.standardize(x)
-            batch_x[i] = x
+                if self.image_data_generator:
+                    # Params need to be set once for every image (not for every mask)
+                    if params is None:
+                        params = self.image_data_generator.get_random_transform(x.shape)
+                    x = self.image_data_generator.apply_transform(x, params)
+                    x = self.image_data_generator.standardize(x)
+                one_hot_map += self.get_one_hot_map(x, k,self.background_color)
+            # If one_hot_map has a max value >1 whe have overlapping classes -> prohibited
+            if one_hot_map.max() > 1:
+                raise ValueError('Mask mismatch: classes are not mutually exclusive (multiple class definitions for '
+                                 'one pixel).')
+            batch_x[i] = one_hot_map
         # optionally save augmented images to disk for debugging purposes
         if self.save_to_dir:
             for i, j in enumerate(index_array):
